feea3b87712fa5670979ad0da774e050d68f7ed6
Mauro Scheltens <mauroscheltens@hotmail.com>
2022-11-25 20:04:02 +0100

    feat: route to index on mafiazone logo

--- resources/views/auth/registration.blade.php
+++ resources/views/auth/registration.blade.php
@@ -10,7 +10,7 @@
 <body>
     <section class="bg-gray-50 dark:bg-gray-900">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+            <a href="{{ route('index') }}" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
                 MafiaZone
             </a>
             <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
