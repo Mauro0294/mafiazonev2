3b4fc05ed22e11171937a245c50e2067a89c0db6
Mauro Scheltens <mauroscheltens@hotmail.com>
2022-12-25 13:35:04 +0100

    feat: desktop index page

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,7 +11,7 @@ class AuthController extends Controller
     public function login()
     {
         return view('auth.login');
-    }  
+    }
       
     public function loginRequest(Request $request)
     {
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,9 +9,9 @@
     <script src="{{ asset('js/menu.js') }}"></script>
 </head>
 <body class="bg-center bg-no-repeat bg-cover m-0 overflow-hidden p-0">
-    <ul class='bg-black flex items-center justify-between h-12 m-0 overflow-hidden pl-12 text-white w-full'>
+    <ul class='bg-black flex items-center justify-between h-12 m-0 overflow-hidden pl-6 uppercase font-bold text-white w-full border-b border-white'>
         <li><i class="fa-solid fa-bars ml-6 block lg:hidden" id='display'></i><i class="fa-solid fa-xmark icon block lg:hidden" id='display2' onclick='closeMenu()'></i><a href='index' class='text-xl'>MafiaZone</a></li>
-        <div class='hide gap-8 flex text-lg'>
+        <div class='hidden lg:flex gap-8 text-lg'>
             <li><a href="#" id="first">Home</a></li>
             <li><a href="login">Game</a></li>
             <li><a href="#">Info</a></li>
@@ -23,10 +23,10 @@
         <li><a href='login'>Game</a></li>
         <li><a href='#'>Info</a></li>
     </ul>
-    <div class='flex flex-col justify-center py-0 px-6 relative h-5/6'>
+    <div class='flex flex-col justify-center py-0 px-6 relative h-5/6 text-white font-bold uppercase'>
         <div>
-            <h1 class="bg-black inline-block p-1 text-center">MafiaZone: Enter the zone</h1><br />
-            <div class='flex justify-center'><a href='register'><button>Create account.</button></a></div>
+            <h1 class="bg-black inline-block p-2 text-center text-3xl">MafiaZone: Enter the zone</h1>
+            <div class='flex justify-center lg:block mt-4'><a href='register'><button class='bg-[#A10216] uppercase px-2 py-1'>Create account.</button></a></div>
         </div>
     </div>
 </body>
